f1b9034b3c1887916a323cc3fa5b75c72b99f9ab
Olli-Pekka Hannukainen <oopee@elisanet.fi>
2022-08-12 08:58:25 +0300

    first commit

---
+++ src/components/Deck.js
@@ -0,0 +1,368 @@
+const Deck = [
+        {
+            id: 1,
+            number: 2,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/two-spades-playing-card-isolated_1308-79142.jpg"
+        },
+        {
+            id: 2,
+            number: 3,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/three-spades-playing-card-isolated_1308-79269.jpg"
+        },
+        {
+            id: 3,
+            number: 4,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/four-spades-playing-card-isolated_1308-78646.jpg"
+        },
+        {
+            id: 4,
+            number: 5,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/five-spades-playing-card-isolated_1308-78294.jpg"
+        },
+        {
+            id: 5,
+            number: 6,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/six-spades-playing-card-isolated_1308-78266.jpg"
+        },
+        {
+            id: 6,
+            number: 7,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/seven-spades-playing-card-isolated_1308-78341.jpg"
+        },
+        {
+            id: 7,
+            number: 8,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/eight-spades-playing-card-isolated_1308-78746.jpg"
+        },
+        {
+            id: 8,
+            number: 9,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/nine-spades-playing-card-isolated_1308-82030.jpg"
+        },
+        {
+            id: 9,
+            number: 10,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/ten-spades-playing-card-isolated_1308-79368.jpg"
+        },
+        {
+            id: 10,
+            number: 11,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/premium-vector/jack-spades-playing-card-isolated_1308-80518.jpg"
+        },
+        {
+            id: 11,
+            number: 12,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/premium-vector/queen-spades-playing-card-isolated_1308-79344.jpg"
+        },
+        {
+            id: 12,
+            number: 13,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/premium-vector/king-spades-playing-card-isolated_1308-79198.jpg"
+        },
+        {
+            id: 13,
+            number: 14,
+            color: "spade",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/ace-spades-playing-card-isolated_1308-78891.jpg"
+        },
+        {
+            id: 14,
+            number: 2,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/two-clubs-playing-card-isolated_1308-78465.jpg"
+        },
+        {
+            id: 15,
+            number: 3,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/three-clubs-playing-card-isolated_1308-79704.jpg"
+        },
+        {
+            id: 16,
+            number: 4,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/four-clubs-playing-card-isolated_1308-80115.jpg"
+        },
+        {
+            id: 17,
+            number: 5,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/five-clubs-playing-card-isolated_1308-78629.jpg"
+        },
+        {
+            id: 18,
+            number: 6,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/six-clubs-playing-card-isolated_1308-80406.jpg"
+        },
+        {
+            id: 19,
+            number: 7,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/seven-clubs-playing-card-isolated_1308-80992.jpg"
+        },
+        {
+            id: 20,
+            number: 8,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/eight-clubs-playing-card-isolated_1308-78567.jpg"
+        },
+        {
+            id: 21,
+            number: 9,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/nine-clubs-playing-card-isolated_1308-78968.jpg"
+        },
+        {
+            id: 22,
+            number: 10,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/ten-clubs-playing-card-isolated_1308-79383.jpg"
+        },
+        {
+            id: 23,
+            number: 11,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/premium-vector/jack-clubs-playing-card-isolated_1308-78355.jpg"
+        },
+        {
+            id: 24,
+            number: 12,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/premium-vector/queen-clubs-playing-card-isolated_1308-79009.jpg"
+        },
+        {
+            id: 25,
+            number: 13,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/premium-vector/king-clubs-playing-card-isolated_1308-78962.jpg"
+        },
+        {
+            id: 26,
+            number: 14,
+            color: "clubs",
+            secondaryColor: "black",
+            iconUrl: "https://img.freepik.com/free-vector/ace-clubs-playing-card-isolated_1308-80924.jpg"
+        },
+        {
+            id: 27,
+            number: 2,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-88776.jpg"
+        },
+        {
+            id: 28,
+            number: 3,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-79074.jpg"
+        },
+        {
+            id: 29,
+            number: 4,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-82069.jpg"
+        },
+        {
+            id: 30,
+            number: 5,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-78990.jpg"
+        },
+        {
+            id: 31,
+            number: 6,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-90597.jpg"
+        },
+        {
+            id: 32,
+            number: 7,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-78291.jpg"
+        },
+        {
+            id: 33,
+            number: 8,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-78766.jpg"
+        },
+        {
+            id: 34,
+            number: 9,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-80471.jpg"
+        },
+        {
+            id: 35,
+            number: 10,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-78919.jpg"
+        },
+        {
+            id: 36,
+            number: 11,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/premium-vector/jack-diamonds-playing-card-isolated_1308-78753.jpg"
+        },
+        {
+            id: 37,
+            number: 12,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/premium-vector/queen-diamonds-playing-card-isolated_1308-78810.jpg"
+        },
+        {
+            id: 38,
+            number: 13,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/premium-vector/king-diamonds-playing-card-isolated_1308-78417.jpg"
+        },
+        {
+            id: 39,
+            number: 14,
+            color: "diamond",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-diamonds-playing-card-isolated_1308-79356.jpg"
+        },
+        {
+            id: 40,
+            number: 2,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/two-hearts-playing-card-isolated_1308-78625.jpg"
+        },
+        {
+            id: 41,
+            number: 3,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/three-hearts-playing-card-isolated_1308-78977.jpg"
+        },
+        {
+            id: 42,
+            number: 4,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/four-hearts-playing-card-isolated_1308-80559.jpg"
+        },
+        {
+            id: 43,
+            number: 5,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/five-hearts-playing-card-isolated_1308-79265.jpg"
+        },
+        {
+            id: 44,
+            number: 6,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/six-hearts-playing-card-isolated_1308-78233.jpg"
+        },
+        {
+            id: 45,
+            number: 7,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/seven-hearts-playing-card-isolated_1308-78196.jpg"
+        },
+        {
+            id: 46,
+            number: 8,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/eight-hearts-playing-card-isolated_1308-79100.jpg"
+        },
+        {
+            id: 47,
+            number: 9,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/nine-hearts-playing-card-isolated_1308-78450.jpg"
+        },
+        {
+            id: 48,
+            number: 10,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ten-hearts-playing-card-isolated_1308-78482.jpg"
+        },
+        {
+            id: 49,
+            number: 11,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/premium-vector/jack-hearts-playing-card-isolated_1308-78574.jpg"
+        },
+        {
+            id: 50,
+            number: 12,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/premium-vector/queen-hearts-playing-card-isolated_1308-78393.jpg"
+        },
+        {
+            id: 51,
+            number: 13,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/premium-vector/king-hearts-playing-card-isolated_1308-78856.jpg"
+        },
+        {
+            id: 52,
+            number: 14,
+            color: "hearts",
+            secondaryColor: "red",
+            iconUrl: "https://img.freepik.com/free-vector/ace-hearts-playing-card-isolated_1308-79361.jpg"
+        }
+]
+
+export default Deck
